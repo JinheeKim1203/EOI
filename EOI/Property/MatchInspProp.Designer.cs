81da117ef731d802b7a241fe550c33d275c5255c
kimjinhee <treasurelady@naver.com>
2025-03-31 13:16:03 +0900

    티칭이미지 리스트, 삭제 ui추가

--- EOI/Property/MatchInspProp.Designer.cs
+++ EOI/Property/MatchInspProp.Designer.cs
@@ -43,6 +43,8 @@
             this.txtExtendX = new System.Windows.Forms.TextBox();
             this.lbX = new System.Windows.Forms.Label();
             this.lbExtent = new System.Windows.Forms.Label();
+            this.flwTeachList = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnDeleteTeachImage = new System.Windows.Forms.Button();
             this.grpMatch.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.picTeachImage)).BeginInit();
             this.SuspendLayout();
@@ -58,93 +60,124 @@
             this.grpMatch.Controls.Add(this.txtExtendX);
             this.grpMatch.Controls.Add(this.lbX);
             this.grpMatch.Controls.Add(this.lbExtent);
-            this.grpMatch.Location = new System.Drawing.Point(3, 3);
+            this.grpMatch.Controls.Add(this.flwTeachList);
+            this.grpMatch.Controls.Add(this.btnDeleteTeachImage);
+            this.grpMatch.Location = new System.Drawing.Point(4, 4);
+            this.grpMatch.Margin = new System.Windows.Forms.Padding(4);
             this.grpMatch.Name = "grpMatch";
-            this.grpMatch.Size = new System.Drawing.Size(296, 265);
+            this.grpMatch.Padding = new System.Windows.Forms.Padding(4);
+            this.grpMatch.Size = new System.Drawing.Size(520, 695);
             this.grpMatch.TabIndex = 0;
             this.grpMatch.TabStop = false;
             this.grpMatch.Text = "패턴매칭";
             // 
             // picTeachImage
             // 
-            this.picTeachImage.Location = new System.Drawing.Point(9, 113);
+            this.picTeachImage.Location = new System.Drawing.Point(13, 170);
+            this.picTeachImage.Margin = new System.Windows.Forms.Padding(4);
             this.picTeachImage.Name = "picTeachImage";
-            this.picTeachImage.Size = new System.Drawing.Size(168, 144);
+            this.picTeachImage.Size = new System.Drawing.Size(240, 216);
             this.picTeachImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.picTeachImage.TabIndex = 7;
             this.picTeachImage.TabStop = false;
             // 
             // txtMatchCount
             // 
-            this.txtMatchCount.Location = new System.Drawing.Point(87, 70);
+            this.txtMatchCount.Location = new System.Drawing.Point(124, 105);
+            this.txtMatchCount.Margin = new System.Windows.Forms.Padding(4);
             this.txtMatchCount.Name = "txtMatchCount";
-            this.txtMatchCount.Size = new System.Drawing.Size(50, 21);
+            this.txtMatchCount.Size = new System.Drawing.Size(70, 28);
             this.txtMatchCount.TabIndex = 5;
             // 
             // lbMatchCount
             // 
             this.lbMatchCount.AutoSize = true;
-            this.lbMatchCount.Location = new System.Drawing.Point(9, 73);
+            this.lbMatchCount.Location = new System.Drawing.Point(13, 110);
+            this.lbMatchCount.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbMatchCount.Name = "lbMatchCount";
-            this.lbMatchCount.Size = new System.Drawing.Size(57, 12);
+            this.lbMatchCount.Size = new System.Drawing.Size(86, 18);
             this.lbMatchCount.TabIndex = 4;
             this.lbMatchCount.Text = "매칭 갯수";
             // 
             // lbScore
             // 
             this.lbScore.AutoSize = true;
-            this.lbScore.Location = new System.Drawing.Point(7, 45);
+            this.lbScore.Location = new System.Drawing.Point(10, 68);
+            this.lbScore.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbScore.Name = "lbScore";
-            this.lbScore.Size = new System.Drawing.Size(65, 12);
+            this.lbScore.Size = new System.Drawing.Size(98, 18);
             this.lbScore.TabIndex = 2;
             this.lbScore.Text = "매칭스코어";
             // 
             // txtExtendY
             // 
-            this.txtExtendY.Location = new System.Drawing.Point(161, 12);
+            this.txtExtendY.Location = new System.Drawing.Point(230, 18);
+            this.txtExtendY.Margin = new System.Windows.Forms.Padding(4);
             this.txtExtendY.Name = "txtExtendY";
-            this.txtExtendY.Size = new System.Drawing.Size(50, 21);
+            this.txtExtendY.Size = new System.Drawing.Size(70, 28);
             this.txtExtendY.TabIndex = 1;
             // 
             // txtScore
             // 
-            this.txtScore.Location = new System.Drawing.Point(87, 42);
+            this.txtScore.Location = new System.Drawing.Point(124, 63);
+            this.txtScore.Margin = new System.Windows.Forms.Padding(4);
             this.txtScore.Name = "txtScore";
-            this.txtScore.Size = new System.Drawing.Size(50, 21);
+            this.txtScore.Size = new System.Drawing.Size(70, 28);
             this.txtScore.TabIndex = 1;
             // 
             // txtExtendX
             // 
-            this.txtExtendX.Location = new System.Drawing.Point(87, 12);
+            this.txtExtendX.Location = new System.Drawing.Point(124, 18);
+            this.txtExtendX.Margin = new System.Windows.Forms.Padding(4);
             this.txtExtendX.Name = "txtExtendX";
-            this.txtExtendX.Size = new System.Drawing.Size(50, 21);
+            this.txtExtendX.Size = new System.Drawing.Size(70, 28);
             this.txtExtendX.TabIndex = 1;
             // 
             // lbX
             // 
             this.lbX.AutoSize = true;
-            this.lbX.Location = new System.Drawing.Point(143, 18);
+            this.lbX.Location = new System.Drawing.Point(204, 27);
+            this.lbX.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbX.Name = "lbX";
-            this.lbX.Size = new System.Drawing.Size(12, 12);
+            this.lbX.Size = new System.Drawing.Size(18, 18);
             this.lbX.TabIndex = 0;
             this.lbX.Text = "x";
             // 
             // lbExtent
             // 
             this.lbExtent.AutoSize = true;
-            this.lbExtent.Location = new System.Drawing.Point(7, 21);
+            this.lbExtent.Location = new System.Drawing.Point(10, 32);
+            this.lbExtent.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbExtent.Name = "lbExtent";
-            this.lbExtent.Size = new System.Drawing.Size(53, 12);
+            this.lbExtent.Size = new System.Drawing.Size(80, 18);
             this.lbExtent.TabIndex = 0;
             this.lbExtent.Text = "확장영역";
             // 
+            // flwTeachList
+            // 
+            this.flwTeachList.AutoScroll = true;
+            this.flwTeachList.Location = new System.Drawing.Point(270, 170);
+            this.flwTeachList.Name = "flwTeachList";
+            this.flwTeachList.Size = new System.Drawing.Size(240, 216);
+            this.flwTeachList.TabIndex = 8;
+            // 
+            // btnDeleteTeachImage
+            // 
+            this.btnDeleteTeachImage.Location = new System.Drawing.Point(270, 400);
+            this.btnDeleteTeachImage.Name = "btnDeleteTeachImage";
+            this.btnDeleteTeachImage.Size = new System.Drawing.Size(240, 32);
+            this.btnDeleteTeachImage.TabIndex = 9;
+            this.btnDeleteTeachImage.Text = "선택 티칭 이미지 삭제";
+            this.btnDeleteTeachImage.Click += new System.EventHandler(this.btnDeleteTeachImage_Click);
+            // 
             // MatchInspProp
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.grpMatch);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "MatchInspProp";
-            this.Size = new System.Drawing.Size(299, 271);
+            this.Size = new System.Drawing.Size(528, 703);
             this.grpMatch.ResumeLayout(false);
             this.grpMatch.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.picTeachImage)).EndInit();
@@ -164,5 +197,7 @@
         private System.Windows.Forms.TextBox txtMatchCount;
         private System.Windows.Forms.Label lbMatchCount;
         private System.Windows.Forms.PictureBox picTeachImage;
+        private System.Windows.Forms.FlowLayoutPanel flwTeachList;
+        private System.Windows.Forms.Button btnDeleteTeachImage;
     }
 }
--- EOI/Property/MatchInspProp.cs
+++ EOI/Property/MatchInspProp.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Security.Policy;
 using System.Text;
@@ -29,6 +30,9 @@ namespace EOI.Property
         public event EventHandler<EventArgs> PropertyChanged;
 
         MatchAlgorithm _matchAlgo = null;
+
+        private string _selectedTemplatePath;  // 👈 현재 선택된 이미지 경로 저장
+
 
         public MatchInspProp()
         {
@@ -66,6 +70,9 @@ namespace EOI.Property
                 Bitmap bmpImage = BitmapConverter.ToBitmap(teachImage);
                 picTeachImage.Image = bmpImage;
             }
+
+            // **jh
+            RefreshTeachImageList();
         }
 
         private void OnUpdateValue(object sender, EventArgs e)
@@ -107,6 +114,119 @@ namespace EOI.Property
             _matchAlgo.MatchCount = matchCount;
 
             PropertyChanged?.Invoke(this, null);
+        }
+
+        private Bitmap SafeLoadImage(string path)
+        {
+            using (var original = Image.FromFile(path))
+            {
+                return new Bitmap(original); // 복사본으로 썸네일 만들기
+            }
+        }
+
+        // **jh
+        private void RefreshTeachImageList()
+        {
+            flwTeachList.Controls.Clear();
+
+            if (_matchAlgo?.LinkedWindow == null)
+                return;
+
+            string uid = _matchAlgo.LinkedWindow.UID;
+            string modelPath = Global.Inst.InspStage.CurModel.ModelPath;
+            if (string.IsNullOrEmpty(modelPath)) return;
+
+            string imgDir = Path.Combine(Path.GetDirectoryName(modelPath), "Images");
+
+            // ⛔ ROI가 없어도 Images 폴더는 없을 수 있음.없으면 그냥 return
+            if (!Directory.Exists(imgDir))
+                return;
+
+            var files = Directory.GetFiles(imgDir, $"{uid}_T*.png");
+
+            foreach (var file in files)
+            {
+                PictureBox thumb = new PictureBox();
+                thumb.Image = SafeLoadImage(file); // ✅ 복사된 이미지 사용
+                thumb.SizeMode = PictureBoxSizeMode.Zoom;
+                thumb.Size = new System.Drawing.Size(60, 60);
+                thumb.Margin = new Padding(5);
+                thumb.Cursor = Cursors.Hand;
+                thumb.Tag = file;
+                thumb.Click += OnTeachThumbnailClick;
+
+                flwTeachList.Controls.Add(thumb);
+            }
+        }
+
+        // **jh
+        private void OnTeachThumbnailClick(object sender, EventArgs e)
+        {
+            PictureBox pb = sender as PictureBox;
+            if (pb?.Tag is string path && File.Exists(path))
+            {
+
+                _selectedTemplatePath = path; // ✅ 경로 저장
+
+                picTeachImage.Image?.Dispose();
+                picTeachImage.Image = Image.FromFile(path);
+            }
+        }
+
+        // **jh
+        private void btnDeleteTeachImage_Click(object sender, EventArgs e)
+        {
+            // 🔐 먼저 경로가 저장돼 있는지 확인
+            if (string.IsNullOrEmpty(_selectedTemplatePath) || !File.Exists(_selectedTemplatePath))
+            {
+                MessageBox.Show("삭제할 이미지를 먼저 선택하세요.");
+                return;
+            }
+
+            try
+            {
+                // 이미지 해제 (파일 핸들 반환)
+                picTeachImage.Image?.Dispose();
+                picTeachImage.Image = null;
+
+                // GC 처리 (강제 해제)
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+
+                // 이미지 파일 삭제
+                File.Delete(_selectedTemplatePath);
+
+                // 경로 초기화
+                _selectedTemplatePath = null;
+
+                // 썸네일 갱신
+                RefreshTeachImageList();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"이미지 삭제 중 오류 발생: {ex.Message}");
+            }
+        }
+
+        // **jh
+        public static class ImageComparer
+        {
+            public static bool IsSameImage(Image img1, Image img2)
+            {
+                if (img1 == null || img2 == null) return false;
+                if (img1.Width != img2.Width || img1.Height != img2.Height) return false;
+
+                using (MemoryStream ms1 = new MemoryStream(), ms2 = new MemoryStream())
+                {
+                    img1.Save(ms1, System.Drawing.Imaging.ImageFormat.Png);
+                    img2.Save(ms2, System.Drawing.Imaging.ImageFormat.Png);
+
+                    byte[] b1 = ms1.ToArray();
+                    byte[] b2 = ms2.ToArray();
+
+                    return b1.SequenceEqual(b2);
+                }
+            }
         }
     }
 }
